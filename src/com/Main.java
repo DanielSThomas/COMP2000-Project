0bb90f3f360d9b991a2403440aa13794aa56778a
DanielSThomas <dansebtom@yahoo.co.uk>
2020-12-26 00:58:36 +0000

    Initial KioskView GUI
    Created base GUI for the kiosk user interface.

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,37 @@
 package com;
 
+import com.view.KioskView;
+
+import javax.swing.*;
+
 public class Main
 {
+    //private JFrame kioskView;
+
+
+
     public static void main(String[] args)
     {
+
+        KioskView kioskView;
+
+        kioskView = new KioskView();
+
+
+
+        //kioskView.setVisible(true);
+
+
+
+
+
+    }
+
+    public void Startup()
+    {
+       // kioskView = new KioskView();
+
+      //  kioskView.isVisible();
 
     }
 }
--- src/com/view/KioskView.java
+++ src/com/view/KioskView.java
@@ -5,12 +5,34 @@ import com.controller.KioskUserController;
 import com.model.Stock;
 import com.model.Item;
 
-public class KioskView {
+import javax.swing.*;
+import java.awt.*;
+
+public class KioskView extends JFrame
+{
     public Stock[] inStock;
 
     public Item[] basketList;
 
     public KioskUserController kioskUserController;
+    private JButton btnFandP;
+    private JButton btnLogin;
+    private JTable tblStock;
+    private JButton btnAddItem;
+    private JTable table1;
+    private JButton btnRemoveItem;
+    private JPanel kioskPanel;
+
+    public KioskView()
+    {
+        this.setContentPane(kioskPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(500,500));
+        this.pack();
+
+        this.setVisible(true);
+    }
+
 
     public void AddToBasket()
     {
@@ -21,5 +43,7 @@ public class KioskView {
     {
 
     }
+
+
 
 }
