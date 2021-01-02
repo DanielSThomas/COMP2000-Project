7c9ee860da355ce1a3cfa07d9341e1444b27a313
DanielSThomas <dansebtom@yahoo.co.uk>
2021-01-02 15:45:35 +0000

    Update ReceiptView
    Has base Jframe functionality.

--- src/com/view/ReceiptView.java
+++ src/com/view/ReceiptView.java
@@ -1,10 +1,28 @@
 package com.view;
 
-import javax.swing.*;
+import com.controller.GUIController;
 
-public class ReceiptView {
+import javax.swing.*;
+import java.awt.*;
+
+public class ReceiptView extends JFrame
+{
     private JButton btnPrint;
     private JButton btnFinish;
     private JList lstReceipt;
     private JPanel receiptPanel;
+
+    private ReceiptView receiptView;
+
+    ReceiptView()
+    {
+        receiptView = this;
+
+        GUIController guiController = new GUIController();
+        receiptView.setContentPane(receiptPanel);
+        receiptView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        receiptView.setPreferredSize(new Dimension(500,500));
+        receiptView.pack();
+
+    }
 }
